ebedbbbb81f1bdf921eeb869f96f4540059a55e7
Kelvin <96843237+kvasily@users.noreply.github.com>
2023-10-26 23:30:27 -0700

    Create Register_or_login.cpp

---
+++ User_Login/Register_or_login.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+
+bool isLoggedIn () 
+{
+    string username, password, un, pw;
+
+    cout << "Enter username: "; cin >> username;
+    cout << "Enter password: "; cin >> password;
+
+    ifstream read(username + ".txt");
+
+    if (!read) 
+    {
+        cout << "User not found\n";
+        cin.get();
+        return false;
+    }
+
+    getline(read, un);
+    getline(read, pw);
+    
+    if (un == username && pw == password) 
+    {
+        return true;
+    } 
+    else 
+    {
+        cout << "Incorrect username or password\n";
+        return false;
+    }
+
+}
+
+int main () 
+{
+    system("clear");
+
+    int choice;
+    cout << "1: Register\n2: Login\nYour choice: "; cin >> choice;
+
+    if (choice == 1) 
+    {
+        string username, password;
+
+        cout << "Select a username: "; cin >> username;
+        cout << "Select a password: "; cin >> password;
+
+        ofstream file(username + ".txt");
+
+        if (!file) 
+        {
+            cout << "Error creating the file";
+            return 1;
+        }
+        cout << "Registration succeful." << endl;
+        file << username << endl << password;
+
+
+    }
+    else if (choice == 2) 
+    {
+        bool status = isLoggedIn();
+
+        if (!status)
+        {
+            cout << "False login!\nPress enter to continue... ";
+            cin.get();
+            return 1;
+        }
+        else 
+        {
+            cout << "Succefully logged in!\nPress enter to continue... ";
+            cin.ignore();
+            cin.get();
+            return 0;
+        }
+    }
+
+}
+
+
