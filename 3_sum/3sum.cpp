9da59b24359a58bb2d7a3cb8b84b4bc8ad521222
Kelvin <96843237+kvasily@users.noreply.github.com>
2023-12-30 15:29:44 -0800

    Create 3sum.cpp

---
+++ 3_sum/3sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Solution 
+{
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) 
+        {
+            for (int i = 0; i < nums.size(); i++)
+            {
+                
+            }
+        }
+    
+};
